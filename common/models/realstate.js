dfbdf4c8e25da35299fe22f7a9ee22304c0d1000
Ahmed Nabil <ahmed@Ahmeds-MacBook-Pro.local>
2016-04-19 22:19:30 +0300

    finish implemnting income details

--- common/models/realstate.js
+++ common/models/realstate.js
@@ -1,5 +1,6 @@
 var Loopback = require('loopback');
 var moment = require('moment');
+var _ = require('underscore');
 
 function getDaysInMonth(m, y) {
     return m===2 ? y & 3 || !(y%25) && y & 15 ? 28 : 29 : 30 + (m+(m>>3)&1);
@@ -80,12 +81,141 @@ module.exports = function(Realstate) {
 					}
 					result.push({ month: month + 1, total: monthSum, allDays: allDays});
 				}
-				// console.log(result);
+
             	cb(err, result);
     		});
 		})
 	
     };
+
+    // Income details
+    // 1. get all contracts within the month
+    //
+    Realstate.incomeDetails = function(id, year, month, cb) {
+    	var Contract = Loopback.getModel('Contract');
+    	var monthFirstDay = year + '-' + month + '-' + '1';
+		var monthLastDay = year + '-' + month + '-' + getDaysInMonth(parseInt(month), parseInt(year));
+    	Contract.find({
+    		where : { and :[
+					{ realstateID: id }, 
+					{
+						and: [
+							{or: [
+							{ startDate: { lte: monthFirstDay }}, 
+							{ startDate: { lte: monthLastDay}} 
+							]},
+							{or: [
+							{ endDate: { gte: monthFirstDay }}, 
+							{ endDate: { gte: monthLastDay}} 
+						]},
+						]
+					}
+					]},
+    		include: ['renewals']
+    	}, function(err, contracts){
+
+    		var monthSum = 0;
+    		var result = {
+    			monthSum: monthSum,
+    			details: []
+    		};
+
+    		// list of contract id's used on renwals filtering
+    		var contractIDsList = [];
+
+    		for(var c = 0; c < contracts.length; c++) {
+    			var con = contracts[c];
+    			var contract = {};
+
+    			//Duration
+    			monthFirstDay = new Date(year, month-1, 1, 12);
+    			monthLastDay = new Date(year, month-1, getDaysInMonth(parseInt(month), parseInt(year)), 12);
+    			var startDate = con.startDate;
+    			var endDate = con.endDate; // RENEWALS!!!!!
+    			
+    			// contract start date within month days
+    			if(startDate >= monthFirstDay && startDate <= monthLastDay) {
+    				contract.startDate = startDate;
+    			} else {
+    				contract.startDate = monthFirstDay;
+    			}
+    			
+    			// contract end date within month days
+    			if(endDate > monthFirstDay && endDate <= monthLastDay) {
+    				contract.endDate = endDate;
+    			} else {
+    				contract.endDate = monthLastDay;
+    			}
+
+    			//Amount - witout brokers or renwals
+    			contract.amount = (con.grandTotal / con.durationInDays) * daysDiff(contract.startDate, contract.endDate);
+    			monthSum += contract.amount;
+    			
+    			// adding contract id to detail
+    			contract.contractID = con.id;
+    			
+    			contractIDsList.push(con.id);
+
+    			result.details.push(contract);
+    		}
+
+    		// Renewals 
+    		var Renewal = Loopback.getModel('Renewal');
+
+    		var filter = {
+	    		where : {
+							and: [
+								{or: [
+								{ startDate: { lte: monthFirstDay }}, 
+								{ startDate: { lte: monthLastDay}} 
+								]},
+								{or: [
+								{ endDate: { gte: monthFirstDay }}, 
+								{ endDate: { gte: monthLastDay}} 
+							]},
+							]
+						},
+	    		include: ['contract']
+    		};
+
+    		Renewal.find(filter, function(err, renewals) {
+
+    			for (var r = 0; r < renewals.length; r++) {
+    				var renewal = renewals[r];
+    				var ren = {};
+    				if (renewal.__data.contract.__data.realstateID !== parseInt(id))
+    					continue;
+
+    				// contract start date within month days
+    				if(renewal.startDate >= monthFirstDay && renewal.startDate <= monthLastDay) {
+    					ren.startDate = renewal.startDate;
+    				} else {
+    					ren.startDate = monthFirstDay;
+    				}
+    				
+    				// contract end date within month days
+    				if(renewal.endDate > monthFirstDay && renewal.endDate <= monthLastDay) {
+    					ren.endDate = renewal.endDate;
+    				} else {
+    					ren.endDate = monthLastDay;
+    				}
+
+    				ren.renewalID = renewal.id;
+    				ren.amount = renewal.total;
+
+    				result.details.push(ren);
+    				monthSum += renewal.total;
+    			}
+	    		result.monthSum = monthSum;
+                result.details = _.sortBy(result.details, function(o) {
+                    return o.startDate;
+                })
+	    		cb(err, result);
+    		})
+
+
+    	});
+    }
 
     Realstate.remoteMethod(
         'income',
@@ -95,6 +225,20 @@ module.exports = function(Realstate) {
             accepts: [	
             			{ arg: 'id', type: 'array', required: true }, 
             			{ arg: 'year', type: 'string', required: true } 
+            		],
+            returns: { arg: 'data', type: ['Realstate'], root: true }
+        }
+    );
+
+    Realstate.remoteMethod(
+        'incomeDetails',
+        {
+            http: { verb: 'get' },
+            description: 'Get income details',
+            accepts: [	
+            			{ arg: 'id', type: 'string', required: true }, 
+            			{ arg: 'year', type: 'string', required: true },
+            			{ arg: 'month', type: 'string', required: true } 
             		],
             returns: { arg: 'data', type: ['Realstate'], root: true }
         }
