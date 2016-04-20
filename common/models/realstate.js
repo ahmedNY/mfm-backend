528979b54e504f2688fb8b7b11f0e8c9ce052b2a
Ahmed Nabil <ahmed@Ahmeds-MacBook-Pro.local>
2016-04-20 15:32:48 +0300

    refine income and income details

--- common/models/realstate.js
+++ common/models/realstate.js
@@ -31,66 +31,125 @@ module.exports = function(Realstate) {
 	// Calculating Realstate income
 	Realstate.income = function (id, year, cb) {
 		Realstate.findById(id, function(err, instance){
-			var rentalFees = instance.rentalFees;
     		var Contract = Loopback.getModel('Contract');
-    		Contract.find(
-    			{ 
-    				where : { realstateID: instance.id }, 
-    				include: ['renewals']
-    			}, function(err, contracts) { 
-    			var result = [];
-    			// 2016
-				for ( var month = 0; month < 12; month++ ) {
-					var monthSum = 0;
-					var allDays = false; // means all days of the month were busy
+            var Renewal = Loopback.getModel('Renewal');
+            var result = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+            Contract.find(
+                { 
+                    where : { realstateID: instance.id }, 
+                    include: ['renewals']
+                }, function(err, contracts) { 
+
+                for ( var month = 0; month < 12; month++ ) {
 					for (var c = 0; c < contracts.length; c++) {
 						var con = contracts[c];
-						//check if the start and end date convering the all month
-						var monthLastDay = getDaysInMonth(month+1, year);
-						console.log('renewals')
-						console.log(con);
-						var endDate = con.endDate;
-						if(con.__data.renewals.length > 0) {
-							endDate = con.__data.renewals[con.__data.renewals.length-1].endDate;
-						}
+                        //check if the start and end date convering the all month
+                        var monthLastDay = getDaysInMonth(month+1, year);
+                        var rentalFees = con.grandTotal / con.durationInDays;
 
 						// contract start on this month and will end on this month
 						if( (con.startDate >= new Date(year, month, 1)) 
-							&& (endDate <= new Date(year, month, monthLastDay))) {
+							&& (con.endDate <= new Date(year, month, monthLastDay))) {
 							// diffrent betwen start and end
-							monthSum += daysDiff(con.startDate, endDate) * rentalFees;
+							result[month] += daysDiff(con.startDate, con.endDate) * rentalFees;
 						} 
 						// contract started before this month and endded in this month
-						else if( ( endDate >= new Date(year, month, 1) ) 
-							&& (endDate <= new Date(year, month, monthLastDay))) {
+						else if( ( con.endDate >= new Date(year, month, 1) ) 
+							&& (con.endDate <= new Date(year, month, monthLastDay))) {
 							// diffrent btween first day of the month and end date
-							monthSum += daysDiff( new Date(year, month, 1), endDate ) * rentalFees;
+							result[month] += daysDiff( new Date(year, month, 1), con.endDate ) * rentalFees;
 						}
 						// contract start on this month and end after this month
 						else if( ( con.startDate >= new Date(year, month, 1) ) 
 							&& (con.startDate <= new Date(year, month, monthLastDay))) {
 							// diffrent between start date and last days of the month
-							monthSum += daysDiff( con.startDate, new Date(year, month, monthLastDay) ) * rentalFees;
+							result[month] += daysDiff( con.startDate, new Date(year, month, monthLastDay) ) * rentalFees;
 						}
 						// contract stared before this month and will end after this month
 						else if( ( con.startDate <= new Date(year, month, 1) ) 
-							&& (endDate >= new Date(year, month, monthLastDay))) {
-							monthSum +=  monthLastDay  * rentalFees;
-							allDays = true;
+							&& (con.endDate >= new Date(year, month, monthLastDay))) {
+							result[month] +=  monthLastDay  * rentalFees;
 						}
 					}
-					result.push({ month: month + 1, total: monthSum, allDays: allDays});
 				}
 
-            	cb(err, result);
-    		});
+                // Renewals 
+                // 1. Get all renewals within year
+                var yearFirstDay = year + '-1-1';
+                var yearLastDay = year + '-12-31';
+
+                var filter = {
+                    where : {
+                        and: [
+                            {or: [
+                                { startDate: { lte: yearFirstDay }}, 
+                                { startDate: { lte: yearLastDay}} 
+                            ]},
+                            {or: [
+                                { endDate: { gte: yearFirstDay }}, 
+                                { endDate: { gte: yearLastDay}} 
+                        ]},
+                        ]
+                    },
+                    include: ['contract']
+                };
+
+                Renewal.find(filter, function(err, renewals) {
+                     for ( var month = 0; month < 12; month++ ) {
+                        for (var r = 0; r < renewals.length; r++) {
+                            var ren = renewals[r];
+
+                            if (ren.__data.contract.__data.realstateID !== parseInt(id))
+                                continue;
+
+                            //check if the start and end date convering the all month
+                            var monthFirstDay = new Date(year, month, 1, 12);
+                            var monthLastDay = new Date(year, month, getDaysInMonth(month+1, year), 12);
+                            var rentalFees = ren.__data.contract.rentalFees;
+
+                            // renewal start on this month and will end on this month
+                            if( (ren.startDate >= monthFirstDay) 
+                                && (ren.endDate <= monthLastDay)) {
+                                // diffrent betwen start and end
+                                result[month] += daysDiff(ren.startDate, ren.endDate) * rentalFees;
+                            } 
+                            // renewal started before this month and endded in this month
+                            else if( ( ren.endDate >= monthFirstDay)  
+                                && (ren.endDate <= monthLastDay)) {
+                                // diffrent btween first day of the month and end date
+                                result[month] += daysDiff( monthFirstDay, ren.endDate ) * rentalFees;
+                            }
+                            // renewal start on this month and end after this month
+                            else if( ( ren.startDate >= monthFirstDay ) 
+                                && (ren.startDate <= monthLastDay)) {
+                                // diffrent between start date and last days of the month
+                                result[month] += daysDiff( ren.startDate, monthLastDay ) * rentalFees;
+                            }
+                            // renewal stared before this month and will end after this month
+                            else if( ( ren.startDate <= monthFirstDay ) 
+                                && (ren.endDate >= monthLastDay)) {
+                                result[month] +=  monthLastDay  * rentalFees;
+                            }
+                        }
+                    }
+                    var formattedResult = [];
+                    for(var m = 0; m < result.length; m++) {
+                        formattedResult.push({
+                            month: m+1,
+                            total:result[m]
+                        });
+                    }
+                    cb(err, formattedResult);
+                })
+    		}); //EOF contracts
+
+            
 		})
 	
     };
 
     // Income details
-    // 1. get all contracts within the month
-    //
     Realstate.incomeDetails = function(id, year, month, cb) {
     	var Contract = Loopback.getModel('Contract');
     	var monthFirstDay = year + '-' + month + '-' + '1';
@@ -111,7 +170,6 @@ module.exports = function(Realstate) {
 						]
 					}
 					]},
-    		include: ['renewals']
     	}, function(err, contracts){
 
     		var monthSum = 0;
@@ -147,9 +205,17 @@ module.exports = function(Realstate) {
     				contract.endDate = monthLastDay;
     			}
 
-    			//Amount - witout brokers or renwals
-    			contract.amount = (con.grandTotal / con.durationInDays) * daysDiff(contract.startDate, contract.endDate);
-    			monthSum += contract.amount;
+
+                //Amount - witout brokers or renwals
+                contract.amount = (con.grandTotal / con.durationInDays) * daysDiff(contract.startDate, contract.endDate);
+                
+                // special case
+                // if the entire month were buzy
+                if(contract.startDate === monthFirstDay && contract.endDate === monthLastDay) {
+    			     contract.amount = (con.grandTotal / con.durationInDays) * getDaysInMonth(parseInt(month), parseInt(year));
+                }
+    			
+                monthSum += contract.amount;
     			
     			// adding contract id to detail
     			contract.contractID = con.id;
@@ -185,15 +251,18 @@ module.exports = function(Realstate) {
     				var ren = {};
     				if (renewal.__data.contract.__data.realstateID !== parseInt(id))
     					continue;
+                    
+                    monthFirstDay = new Date(year, month-1, 1, 12);
+                    monthLastDay = new Date(year, month-1, getDaysInMonth(parseInt(month), parseInt(year)), 12);
 
-    				// contract start date within month days
+    				// renewal start date within month days
     				if(renewal.startDate >= monthFirstDay && renewal.startDate <= monthLastDay) {
     					ren.startDate = renewal.startDate;
     				} else {
     					ren.startDate = monthFirstDay;
     				}
     				
-    				// contract end date within month days
+    				// renewal end date within month days
     				if(renewal.endDate > monthFirstDay && renewal.endDate <= monthLastDay) {
     					ren.endDate = renewal.endDate;
     				} else {
@@ -201,10 +270,10 @@ module.exports = function(Realstate) {
     				}
 
     				ren.renewalID = renewal.id;
-    				ren.amount = renewal.total;
+    				ren.amount = daysDiff(ren.startDate, ren.endDate) * renewal.__data.contract.rentalFees;
 
     				result.details.push(ren);
-    				monthSum += renewal.total;
+    				monthSum += ren.amount;
     			}
 	    		result.monthSum = monthSum;
                 result.details = _.sortBy(result.details, function(o) {
